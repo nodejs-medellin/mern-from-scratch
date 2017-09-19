1f17e7e93f405affa66a79692b4d99d2d52186e3
kerosine <camilomontoya2@gmail.com>
2017-09-19 16:31:55 -0500

    MascotasList empty template

---
+++ src/components/MascotaList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MascotasList = () => {
+  return (
+    <div>
+      <h1>Mascotas list component</h1>
+    </div>
+  );
+};
+
+MascotasList.propTypes = {};
+
+MascotasList.defaultTypes = {};
+
+export default MascotasList;
